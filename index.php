96ebe4a9322bb4f34e941d95b3d5ca0a98a5e0fc
CarloSalaris <carloa.salaris@gmail.com>
2023-06-21 16:35:44 +0200

    Variable $pwdLength gets the input value with GET

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Strong Password Generator</title>
+
+    <?php
+
+        $pwdLength = $_GET['inputLength'];
+        var_dump($pwdLength);
+
+    ?>
 </head>
 
 <body>
@@ -21,12 +28,17 @@
     </div>
 
     <!-- FORM -->
-    <form>
+    <form method="get" >
         <!-- ROW Lunghezza password -->
         <div class="row mb-3 space-between">
             <label for="inputEmail3" class="col-sm-7 col-form-label"for="pwdLength">Lunghezza Password: </label>
             <div class="col-sm-4">
-                <input type="number" class="form-control" id="inputLength">
+                <input type="number" class="form-control" name="inputLength" id="inputLength">
+            </div>
+            <!-- BUTTONS -->
+            <div>
+                <button type="submit" class="btn btn-primary">Invia</button>
+                <button type="reset" class="btn btn-secondary">Annulla</button>
             </div>
         </div>
     </form>
