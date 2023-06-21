6ef061c63fc23331ca676ba1e2cfd3e6eab1b75d
CarloSalaris <carloa.salaris@gmail.com>
2023-06-21 19:19:23 +0200

    At least one character of each kind in password

--- index.php
+++ index.php
@@ -19,9 +19,14 @@
         $uppercase = array_flip(range('A', 'Z')); 
         $symbols = array_flip(str_split('"!@#$%^&*()_+=-{}[]\/|;:<>?/' . "'"));
         $combined = array_merge($numbers, $lowercase, $uppercase, $symbols);
-        //implode to return a string
-        $password = implode(array_rand($combined, $pwdLength));
-
+        //implode to return a string (if extracting an array instead of a string)
+        //at least one of each kind
+        //str_shuffle so the order is random
+        $password = str_shuffle(array_rand($numbers) . 
+                                array_rand($lowercase) . 
+                                array_rand($uppercase) . 
+                                array_rand($symbols) . 
+                                implode(array_rand($combined, $pwdLength - 4)));
     ?>
 </head>
 
